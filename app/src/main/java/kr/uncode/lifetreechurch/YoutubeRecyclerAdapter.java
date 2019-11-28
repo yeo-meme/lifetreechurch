72cf656f30bd369ac21a6b87cf6935d44f178588
yeomimKim <yeomeme@gmail.com>
2019-11-28 15:50:16 +0900

    YoutubeListFragment 라이브러리 데모 동영상 리사이클러뷰 적용 (4가지 동영상 메뉴가 1개의 xml을 동시 적용 받고 있다)

---
+++ app/src/main/java/kr/uncode/lifetreechurch/YoutubeRecyclerAdapter.java
@@ -0,0 +1,101 @@
+package kr.uncode.lifetreechurch;
+
+import android.app.Activity;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class YoutubeRecyclerAdapter extends RecyclerView.Adapter<BaseViewHolder> {
+
+    public static final int VIEW_TYPE_NOMAL = 1;
+    private List<YoutubeVideoModel> mYoutubeVideos;
+    DisplayMetrics displayMetrics = new DisplayMetrics();
+
+    public YoutubeRecyclerAdapter(List<YoutubeVideoModel> youtubeVideos) {
+        mYoutubeVideos = youtubeVideos;
+    }
+
+    @NonNull
+    @Override
+    public BaseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_youtube_list, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BaseViewHolder holder, int position) {
+        holder.onBind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mYoutubeVideos != null &&
+                mYoutubeVideos.size() > 0) {
+            return mYoutubeVideos.size();
+        } else {
+            return 1;
+        }
+    }
+
+    public void setItems(List<YoutubeVideoModel> youtubeVideos) {
+        mYoutubeVideos = youtubeVideos;
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends BaseViewHolder {
+
+        @BindView(R.id.textViewTitle)
+        TextView textWaveTitle;
+        @BindView(R.id.btnPlay)
+        ImageView playButton;
+
+        @BindView(R.id.imageViewItem)
+        ImageView imageViewItems;
+        @BindView(R.id.youtube_view)
+        YouTubePlayerView youTubePlayerView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        @Override
+        protected void clear() {
+        }
+
+        public void onBind(int position) {
+            super.onBind(position);
+            final YoutubeVideoModel mYoutubeVideo = mYoutubeVideos.get(position);
+            ((Activity) itemView.getContext()).getWindowManager()
+                    .getDefaultDisplay()
+                    .getMetrics(displayMetrics);
+            int width = displayMetrics.widthPixels;
+            if (mYoutubeVideo.getTitle() != null) {
+                textWaveTitle.setText(mYoutubeVideo.getTitle());
+            }
+
+
+            if (mYoutubeVideo.getImageUrl() != null) {
+                Glide.with(itemView.getContext())
+                        .load(mYoutubeVideo.getImageUrl())
+                        .apply(new RequestOptions().override(width - 36, 200))
+                        .into(imageViewItems);
+            }
+        }
+    }
+}
--- app/src/main/java/kr/uncode/lifetreechurch/fm_video/VideoListFragment.java
+++ app/src/main/java/kr/uncode/lifetreechurch/fm_video/VideoListFragment.java
@@ -1,5 +1,6 @@
 package kr.uncode.lifetreechurch.fm_video;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,19 +9,37 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import kr.uncode.lifetreechurch.R;
+import kr.uncode.lifetreechurch.YoutubeRecyclerAdapter;
+import kr.uncode.lifetreechurch.YoutubeVideoModel;
 import kr.uncode.lifetreechurch.base.BaseFragment;
 import kr.uncode.lifetreechurch.databinding.FmVideolistBinding;
+import kr.uncode.lifetreechurch.lt_main.MainActivity;
 
 public class VideoListFragment extends BaseFragment {
 
+    YoutubeRecyclerAdapter mRecyclerAdapter;
+//    @BindView(R.id.recyclerViewFeed)
+     private RecyclerView recyclerViewFeed;
+    private RecyclerView.LayoutManager mLayoutManager;
     private YouTubePlayerView youTubePlayerViewLayout;
     FmVideolistBinding binding;
+
+
+
 
     @Nullable
     @Override
@@ -28,9 +47,13 @@ public class VideoListFragment extends BaseFragment {
 //데이터바인딩 유튜트 라이브러리 적용 어려워서 일단 기본틀로 가려고 주석
 //        binding = DataBindingUtil.inflate(inflater, R.layout.fm_videolist, container, false);
 
-        View view = inflater.inflate(R.layout.fm_videolist,container,false);
+        View view = inflater.inflate(R.layout.fm_videolist, container, false);
         youTubePlayerViewLayout = view.findViewById(R.id.youtube_player_view);
+        recyclerViewFeed = view.findViewById(R.id.recyclerViewFeed);
 
+        recyclerViewFeed.setLayoutManager(mLayoutManager);
+        recyclerViewFeed.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewFeed.setAdapter(mRecyclerAdapter);
         return view;
     }
 
@@ -41,9 +64,70 @@ public class VideoListFragment extends BaseFragment {
             @Override
             public void onReady(YouTubePlayer youTubePlayer) {
                 super.onReady(youTubePlayer);
-                String videoId ="f3aI5OUbHKE";
-                youTubePlayer.loadVideo(videoId,0);
+                String videoId = "f3aI5OUbHKE";
+                youTubePlayer.loadVideo(videoId, 0);
             }
         });
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Activity activity = getActivity();
+        if (activity != null && activity instanceof MainActivity)
+            ButterKnife.bind(getActivity());
+
+
+        List<YoutubeVideoModel> youtubeVideo = preapreList();
+        mRecyclerAdapter = new YoutubeRecyclerAdapter(youtubeVideo);
+        mLayoutManager =new LinearLayoutManager(getActivity());
+
+    }
+    private List<YoutubeVideoModel> preapreList() {
+        ArrayList<YoutubeVideoModel> videoArrayList = new ArrayList<>();
+
+        //add 데모 아이템
+        YoutubeVideoModel video1 = new YoutubeVideoModel();
+        video1.setId(1l);
+        video1.setImageUrl("https://i.ytimg.com/vi/zI-Pux4uaqM/maxresdefault.jpg");
+        video1.setTitle(
+                "Thugs Of Hindostan - Official Trailer | Amitabh Bachchan | Aamir Khan");
+        video1.setVideoId("zI-Pux4uaqM");
+        videoArrayList.add(video1);
+
+        // add second item
+        YoutubeVideoModel video2 = new YoutubeVideoModel();
+        video2.setId(2l);
+        video2.setImageUrl("https://i.ytimg.com/vi/8ZK_S-46KwE/maxresdefault.jpg");
+        video2.setTitle(
+                "Colors for Children to Learning with Baby Fun Play with Color Balls Dolphin...");
+        video2.setVideoId("8ZK_S-46KwE");
+
+        // add third item
+        YoutubeVideoModel video3 = new YoutubeVideoModel();
+        video3.setId(3l);
+        video3.setImageUrl("https://i.ytimg.com/vi/8czMWUH7vW4/hqdefault.jpg");
+        video3.setTitle("Air Hostess Accepts Marriage Proposal Mid-Air, Airline Fires her.");
+        video3.setVideoId("8czMWUH7vW4");
+
+        // add four item
+        YoutubeVideoModel video4 = new YoutubeVideoModel();
+        video4.setId(4l);
+        video4.setImageUrl("https://i.ytimg.com/vi/YrQVYEb6hcc/maxresdefault.jpg");
+        video4.setTitle("EXPERIMENT Glowing 1000 degree METAL BALL vs Gunpowder (100 grams)");
+        video4.setVideoId("YrQVYEb6hcc");
+
+        // add four item
+        YoutubeVideoModel video5 = new YoutubeVideoModel();
+        video5.setId(5l);
+        video5.setImageUrl("https://i.ytimg.com/vi/S84Fuo2rGoY/maxresdefault.jpg");
+        video5.setTitle("What happened after Jauhar of Padmavati");
+        video5.setVideoId("S84Fuo2rGoY");
+
+        videoArrayList.add(video1);
+        videoArrayList.add(video2);
+        videoArrayList.add(video3);
+        videoArrayList.add(video4);
+        return videoArrayList;
     }
 }

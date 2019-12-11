8b2ba7641ecab1c9ccbd7df44d022064d08c27ff
yeomimKim <yeomeme@gmail.com>
2019-12-11 16:33:31 +0900

    주보 디테일뷰 들어가서 한장으로 보기 and 핀치줌 적용 OK (해상도가 안좋아서 핀치줌을 했을때 이미지의 글자가 제대로 보이지 않음)

---
+++ app/src/main/java/kr/uncode/lifetreechurch/WeeklyFm/WeeklyImage1Fm.java
@@ -0,0 +1,61 @@
+package kr.uncode.lifetreechurch.WeeklyFm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import kr.uncode.lifetreechurch.R;
+import kr.uncode.lifetreechurch.base.BaseFragment;
+import kr.uncode.lifetreechurch.databinding.FmWeekly1Binding;
+import kr.uncode.lifetreechurch.utils.MLog;
+
+public class WeeklyImage1Fm extends BaseFragment {
+    private static final String IMAGE_URL = "WEEKLY_IMAGE";
+
+    private String image_url;
+
+    FmWeekly1Binding binding;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fm_weekly1, container, false);
+//        binding = FragmentMainBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) {
+            image_url = getArguments().getString(IMAGE_URL); // 전달한 key 값
+            MLog.d(image_url);
+        }
+        getWeeklyImg();
+    }
+
+    private void getWeeklyImg() {
+        if (image_url != null) {
+            Glide.with(binding.getRoot())
+                    .load(image_url)
+                    .override(500,300)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(binding.weeklyImage1);
+
+        }
+
+    }
+}
--- app/src/main/java/kr/uncode/lifetreechurch/WeeklyFm/Weekly_Fm.java
+++ app/src/main/java/kr/uncode/lifetreechurch/WeeklyFm/Weekly_Fm.java
@@ -9,6 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -19,11 +21,13 @@ import kr.uncode.lifetreechurch.Config.JiNuConfig;
 import kr.uncode.lifetreechurch.Model.BlogWeekly;
 import kr.uncode.lifetreechurch.R;
 import kr.uncode.lifetreechurch.ResponseCallback;
+import kr.uncode.lifetreechurch.base.BaseFragment;
 import kr.uncode.lifetreechurch.databinding.FmWeeklyBinding;
+import kr.uncode.lifetreechurch.fm_video.VideoFragment;
 import kr.uncode.lifetreechurch.utils.MLog;
 
-public class Weekly_Fm extends Fragment {
-
+public class Weekly_Fm extends BaseFragment {
+    private static final String IMAGE_URL = "WEEKLY_IMAGE";
     private String weekly1Url;
     private String weekly2Url;
     private JiNuConfig jinuConfig;
@@ -34,8 +38,40 @@ public class Weekly_Fm extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fm_weekly, container, false);
+        ClickCheck();
         return binding.getRoot();
     }
+
+    private void ClickCheck() {
+        binding.weeklyImage1.setOnClickListener(this::weekly1DetailView);
+        binding.weeklyImage2.setOnClickListener(this::weekly2DetailView);
+    }
+
+
+    private void weekly1DetailView(View view) {
+        MLog.d();
+        Fragment fragment = new WeeklyImage1Fm(); // Fragment 생성
+        Bundle bundle = new Bundle(1); // 파라미터는 전달할 데이터 개수
+        bundle.putString(IMAGE_URL, weekly1Url); // key , value
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fmt = fm.beginTransaction();
+        fragment.setArguments(bundle);
+        fmt.replace(R.id.main_container,fragment).addToBackStack(null).commit();
+    }
+
+
+    private void weekly2DetailView(View view) {
+        MLog.d();
+        Fragment fragment = new WeeklyImage2Fm(); // Fragment 생성
+        Bundle bundle = new Bundle(1); // 파라미터는 전달할 데이터 개수
+        bundle.putString(IMAGE_URL, weekly2Url); // key , value
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fmt = fm.beginTransaction();
+        fragment.setArguments(bundle);
+        fmt.replace(R.id.main_container,fragment).addToBackStack(null).commit();
+
+    }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
